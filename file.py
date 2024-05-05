60c0761dc32d8fc84b340777c4e65a09d7a8993b
Ariffazeel99 <arif.fazeel99@gmail.com>
2024-05-05 20:19:49 +0200

    working with env

--- file.py
+++ file.py
@@ -1,8 +1,17 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-# Replace with your actual Page Access Token and Page ID
-PAGE_ACCESS_TOKEN = ''
-PAGE_ID = '101569698587025'
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve environment variables
+PAGE_ACCESS_TOKEN = os.getenv('PAGE_ACCESS_TOKEN')
+PAGE_ID = os.getenv('PAGE_ID')
+
+# Ensure both environment variables are set
+if not PAGE_ACCESS_TOKEN or not PAGE_ID:
+    raise ValueError("Please ensure PAGE_ACCESS_TOKEN and PAGE_ID are set in the .env file.")
 
 # Graph API URL with the fields you want to retrieve
 url = f'https://graph.facebook.com/v17.0/{PAGE_ID}?fields=id,name,about,fan_count&access_token={PAGE_ACCESS_TOKEN}'
